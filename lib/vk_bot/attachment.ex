bc42d9ee57618c5bddb1b9307d805de1097cd1e3
Evgeny Rybin <z.ribin20@gmail.com>
2022-12-19 23:26:48 +0500

    Add struct to `Attachment`

--- lib/vk_bot/attachment.ex
+++ lib/vk_bot/attachment.ex
@@ -1,30 +1,37 @@
-defmodule VkBot.Attachments do
-  def download(attachment) do
+defmodule VkBot.Attachment do
+  @can_download ~w[photo video audio doc sticker]
+
+  defstruct ~w[type object url file]a
+
+  def new(%{"type" => type} = attachment) when type in @can_download do
+    object = attachment[type]
+
+    %__MODULE__{
+      type: String.to_atom(type),
+      object: object
+    }
+  end
+
+  def download(%__MODULE__{} = attachment) do
     attachment
     |> get_url()
     |> download_by_url()
   end
 
-  defp download_by_url(%{"url" => url, "type" => type}) do
+  defp download_by_url(%__MODULE__{url: url} = attachment) do
     body =
       HTTPoison.get!(url)
       |> Map.fetch!(:body)
 
-    %{"file" => body, "type" => type}
+    Map.put(attachment, :file, body)
   end
 
-  defp fetch_object(attachment) do
-    attachment[attachment["type"]]
-  end
-
-  defp get_url(%{"type" => "photo"} = attachment) do
-    obj = fetch_object(attachment)
-
+  defp get_url(%__MODULE__{type: :photo, object: object} = attachment) do
     url =
-      obj["sizes"]
+      object["sizes"]
       |> Enum.max_by(fn s -> s["width"] * s["height"] end)
       |> Map.fetch!("url")
 
-    %{"type" => "photo", "url" => url}
+    Map.put(attachment, :url, url)
   end
 end
