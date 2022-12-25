2708d4b5b546cf9e404d8dba40a5e2dc882384f6
Evgeny Rybin <z.ribin20@gmail.com>
2022-12-25 14:08:06 +0500

    Add :in, only_admin: options

--- lib/vk_bot/commands_manager.ex
+++ lib/vk_bot/commands_manager.ex
@@ -1,4 +1,6 @@
 defmodule VkBot.CommandsManager do
+  @default_options %{only_admin: false, in: :pm}
+
   defmacro __using__(_opts) do
     quote do
       require VkBot.CommandsManager
@@ -21,14 +23,10 @@ defmodule VkBot.CommandsManager do
     end
   end
 
-  defmacro defcommand(arg, [on_text: text], do: body) do
+  defmacro defcommand(arg, opts, do: body) do
     quote do
       def predicate(event) do
-        text = fetch_text(event)
-
-        text
-        |> String.downcase()
-        |> String.starts_with?(unquote(text))
+        VkBot.CommandsManager.predicate(event, unquote(opts))
       end
 
       def handle_event(unquote(arg)) do
@@ -36,6 +34,53 @@ defmodule VkBot.CommandsManager do
       end
     end
   end
+
+  def predicate(event, opts) do
+    opts = Enum.into(opts, @default_options)
+
+    [
+      &predicate_on_text/2,
+      &predicate_only_admin/2,
+      &predicate_in/2
+    ]
+    |> Enum.map(fn fun -> fun.(event, opts) end)
+    |> Enum.all?()
+  end
+
+  defp predicate_on_text(event, %{on_text: on_text}) do
+    text = fetch_text(event)
+
+    text
+    |> String.downcase()
+    |> String.starts_with?(on_text)
+  end
+
+  defp predicate_on_text(_event, _opts), do: true
+
+  defp predicate_only_admin(event, %{only_admin: true}) do
+    %{"peer_id" => peer_id, "from_id" => from_id} = fetch_message(event)
+
+    VkBot.Api.exec_method("messages.getConversationMembers", %{"peer_id" => peer_id})
+    |> Map.fetch!("items")
+    |> Enum.find(%{}, fn user -> Map.fetch!(user, "member_id") == from_id end)
+    |> Map.get("is_admin", false)
+  end
+
+  defp predicate_only_admin(_event, _opts), do: true
+
+  defp predicate_in(event, %{in: :chat}) do
+    %{"peer_id" => peer_id, "from_id" => from_id} = fetch_message(event)
+
+    peer_id != from_id
+  end
+
+  defp predicate_in(event, %{in: :pm}) do
+    %{"peer_id" => peer_id, "from_id" => from_id} = fetch_message(event)
+
+    peer_id == from_id
+  end
+
+  defp predicate_in(_event, _opts), do: true
 
   def fetch_text(update) do
     update
@@ -57,6 +102,7 @@ defmodule VkBot.CommandsManager do
 
   def handle_response({:ok, text}, event) when is_binary(text) do
     peer_id = fetch_peer_id(event)
+
     VkBot.Api.exec_method("messages.send", %{
       "peer_id" => peer_id,
       "random_id" => 0,
--- lib/vk_bot/examples/ping_pong_bot_commands/pint_pong_command.ex
+++ lib/vk_bot/examples/ping_pong_bot_commands/pint_pong_command.ex
@@ -1,7 +1,7 @@
 defmodule VkBot.Examples.PingPongCommand do
   use VkBot.CommandsManager
 
-  defcommand _event, on_text: "/ping" do
+  defcommand _event, on_text: "/ping", only_admin: true, in: :chat do
     {:ok, "pong"}
   end
 end
